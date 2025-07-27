80fce16cfa470fb87d3e124149c5223884e4b922
DN5S <me@dn5s.dev>
2025-07-27 20:07:47 +0900

    Migrate "SamplePlugin" to "TataruLink" with updated namespaces, configurations, and dependencies.

--- TataruLink/Configuration.cs
+++ TataruLink/Configuration.cs
@@ -1,8 +1,7 @@
-﻿using Dalamud.Configuration;
-using Dalamud.Plugin;
-using System;
+﻿using System;
+using Dalamud.Configuration;
 
-namespace SamplePlugin;
+namespace TataruLink;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
