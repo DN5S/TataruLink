d518c69f1d54602b62876f0a8446e9eeab6648d8
DN5S <me@dn5s.dev>
2025-07-30 10:25:37 +0900

    docs: Add file reference for `XivChatTypeHelper.cs`

--- TataruLink/Utils/XivChatTypeHelper.cs
+++ TataruLink/Utils/XivChatTypeHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿// File: TataruLink/Utils/XivChatTypeHelper.cs
+using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Game.Text;
 using TataruLink.Localization;
